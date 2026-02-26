8d420c1f146342f9c7d7a289b705b482a9b443a0
GoofyKetchup <levraimerwane@gmail.com>
2026-02-26 17:06:47 +0100

    Refactor to enable protected mode and print message

--- boot/protected.asm
+++ boot/protected.asm
@@ -2,41 +2,43 @@
 [ORG 0x8000]
 
 start:
+    mov ah, 0x0E
+    mov al, 'A'
+    int 0x10
     cli
     mov eax, cr0
     or eax, 1
     mov cr0, eax
-    call protected_mode_start
-    mov si, msg2
-    call print_string
+    jmp 0x08:protected_mode_start_32
 
-print_string:
-    lodsb
-.next_char:
-    cmp al, 0
-    je .done
-
-    mov ah, 0x0E
-    int 0x0E
-    jmp .next_char
-.done:
-    mov ah, 0x0E
-    mov al, 0x0D
-    int 0x10
-    mov al, 0x0A
-    int 0x10
-    ret
-
-hang:
-    jmp hang
-
-msg2 db "Protected Mode Enabled.",0
-
-protected_mode_start:
+[BITS 32]
+protected_mode_start_32:
     mov ax, 0x10
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
+    mov esp, 0x90000
+
+    mov esi, msg2
+    mov edi, 0xB8000
+    call print_string_32
+
+hang:
+    jmp hang
+
+print_string_32:
+.next_char:
+    lodsb
+    cmp al, 0
+    je .done
+
+    mov ah, 0x0F
+    mov [edi], ax
+    add edi, 2
+    jmp .next_char
+.done:
     ret
+
+msg2 db "Protected Mode Enabled.",0
